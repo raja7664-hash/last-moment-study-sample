c9139d8f97d74231017b1eedcb0d8cde4888637e
vikram-hub <vikram.yadav@s-force.org>
2020-01-04 18:13:10 +0530

    code changed

--- src/main/java/com/spring/secureApp/HomeController.java
+++ src/main/java/com/spring/secureApp/HomeController.java
@@ -1,11 +1,11 @@
 package com.spring.secureApp;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String home() {
 		System.out.println("HomeController.home()");
 		return "home";
--- src/main/java/com/spring/secureApp/SecureApplication.java
+++ src/main/java/com/spring/secureApp/SecureApplication.java
@@ -4,13 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecureAppApplication {
-		 
-	   
+public class SecureApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecureAppApplication.class, args);
-	
-		
+		SpringApplication.run(SecureApplication.class, args);
 	}
-
 }
