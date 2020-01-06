fdde807a2e695ae06d4edb4a9f7b4f49ceda9a7b
vikram-hub <vikram.yadav@s-force.org>
2020-01-06 22:03:18 +0530

    change code

--- src/main/java/com/spring/secureApp/HomeController.java
+++ src/main/java/com/spring/secureApp/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/hello")
 	public String home() {
-		System.out.println("Running from Docker....");
+		System.out.println("Running from Docker.!!!!");
 		System.out.println("HomeController.home()");
 		return "home";
 	}
